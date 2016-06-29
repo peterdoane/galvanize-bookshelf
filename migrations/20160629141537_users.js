912917dda9b734478a1f34f9267b795dc11265af
Peter Doane <peterdoane@gmail.com>
2016-06-29 14:38:24 -0700

    commit

---
+++ migrations/20160629141537_users.js
@@ -0,0 +1,16 @@
+'use strict';
+
+exports.up = function(knex) {
+  return knex.schema.createTable('users', (table) => {
+    table.increments();
+    table.string('first_name').notNullable().default('');
+    table.string('last_name').notNullable().default('');
+    table.string('email').unique().notNullable();
+    table.specificType('hashed_password', 'char(60)').notNullable();
+    table.timestamps(true,true);
+  });
+};
+
+exports.down = function(knex) {
+  return knex.schema.dropTable('users');
+};
--- routes/users.js
+++ routes/users.js
@@ -1,8 +1,28 @@
 'use strict';
 
-const express = require('express');
-const router = express.Router();
-
-// YOUR CODE HERE
-
-module.exports = router;
+// const express = require('express');
+// const router = express.Router();
+// const bcrypt = require('bcrypt');
+// const knex = require('../knex');
+//
+// router.post('/users', (req, res, next) => {
+//   bcrypt.hash(req.body.password, 12, (err, hashed_password) => {
+//     if (err) {
+//       return next(err);
+//     }
+//
+//     knex('users')
+//       .insert({
+//         email: req.body.email,
+//         hashed_password: hashed_password
+//       }, '*')
+//       .then((users) => {
+//         res.sendStatus(200);
+//       })
+//       .catch((err) => {
+//         next(err);
+//       });
+//   });
+// });
+//
+// module.exports = router;

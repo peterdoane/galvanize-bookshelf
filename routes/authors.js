4c2515c7826edb64160d5b90ef566e00998d9798
Peter Doane <peterdoane@gmail.com>
2016-06-29 14:01:48 -0700

    commit

--- routes/authors.js
+++ routes/authors.js
@@ -1,8 +1,72 @@
-'use strict';
+'use strict'
 
 const express = require('express');
 const router = express.Router();
+const knex = require('../knex');
 
-// YOUR CODE HERE
+router.get('/authors', (_req,res, next) => {
+  knex('authors')
+    .orderBy('id')
+    .then((authors) => {
+    res.send(authors);
+    })
+    .catch((err) => {
+      next(err);
+    })
+});
 
+router.get('/authors/:id', (req, res, next) => {
+  knex('authors')
+    .where('id',req.params.id)
+    .first()
+    .then((author) => {
+      res.send(author);
+    })
+    .catch((err) => {
+      next(err);
+    });
+});
+
+router.post('/authors', (req, res, next) => {
+  knex('authors')
+    .insert(req.body, '*')
+    .then((authors) => {
+      res.send(authors[0])
+    })
+    .catch((err) => {
+      next(err)
+    });
+});
+
+router.patch('/authors/:id', (req,res,next)=>{
+  knex('authors')
+    .update(req.body, '*')
+    .where('id', req.params.id)
+    .then((authors) => {
+      res.send(authors[0]);
+    })
+    .catch((err)=> {
+      next(err);
+    });
+});
+
+router.delete('/authors/:id', (req,res, next) =>{
+  knex('authors')
+    .where('id', req.params.id)
+    .first()
+    .then((author) => {
+
+      return knex('authors')
+        .del()
+        .where('id', req.params.id)
+        .then(() => {
+          delete author.id;
+          res.send(author)
+        });
+
+    })
+    .catch((err) =>{
+      next(err);
+    });
+});
 module.exports = router;
--- routes/books.js
+++ routes/books.js
@@ -2,7 +2,111 @@
 
 const express = require('express');
 const router = express.Router();
+const knex = require('../knex');
 
-// YOUR CODE HERE
+router.get('/books', (_req, res, next) => {
+  knex('books')
+    .orderBy('id')
+    .then((books) => {
+      res.send(books);
+    })
+    .catch((err) => {
+      next(err);
+    });
+});
+
+router.get('/books/:id', (req, res, next) => {
+  knex('books')
+    .where('id', req.params.id)
+    .first()
+    .then((book) => {
+      if (!book) {
+        return next();
+      }
+
+      res.send(book);
+    })
+    .catch((err) => {
+      next(err);
+    });
+});
+
+router.post('/books', (req, res, next) => {
+  knex('authors')
+    .where('id', req.body.artist_id)
+    .first()
+    .then((artist) => {
+      if (!artist) {
+        return res
+          .status(400)
+          .set('Content-Type', 'text/plain')
+          .send('artist_id does not exist');
+      }
+
+      return knex('books')
+        .insert(req.body, '*')
+        .then((results) => {
+          res.send(results[0]);
+        });
+    })
+    .catch((err) => {
+      next(err);
+    });
+});
+
+router.patch('/books/:id', (req, res, next) => {
+  knex('books')
+    .where('id', req.params.id)
+    .first()
+    .then((book) => {
+      if (!book) {
+        return next();
+      }
+
+      return knex('authors')
+        .where('id', req.body.artist_id)
+        .first()
+        .then((artist) => {
+          if (!artist) {
+            return res
+              .status(400)
+              .set('Content-Type', 'text/plain')
+              .send('artist_id does not exist');
+          }
+
+          return knex('books')
+            .update(req.body, '*')
+            .where('id', req.params.id)
+            .then((results) => {
+              res.send(results[0]);
+            });
+        });
+    })
+    .catch((err) => {
+      next(err);
+    });
+});
+
+router.delete('/books/:id', (req, res, next) => {
+  knex('books')
+    .where('id', req.params.id)
+    .first()
+    .then((book) => {
+      if (!book) {
+        return next();
+      }
+
+      return knex('books')
+        .del()
+        .where('id', req.params.id)
+        .then(() => {
+          delete book.id;
+          res.send(book);
+        });
+    })
+    .catch((err) => {
+      next(err);
+    });
+});
 
 module.exports = router;

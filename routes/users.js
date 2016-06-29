53b535825a1a028e2389e484f496cf17d487d287
Peter Doane <peterdoane@gmail.com>
2016-06-29 15:09:51 -0700

    commit

--- routes/users.js
+++ routes/users.js
@@ -1,21 +1,36 @@
 'use strict';
 
-// const express = require('express');
-// const router = express.Router();
-// const bcrypt = require('bcrypt');
-// const knex = require('../knex');
-//
-// router.post('/users', (req, res, next) => {
-//   bcrypt.hash(req.body.password, 12, (err, hashed_password) => {
-//     if (err) {
-//       return next(err);
-//     }
-//
-//     knex('users')
-//       .insert({
-//         email: req.body.email,
-//         hashed_password: hashed_password
-//       }, '*')
+const express = require('express');
+const router = express.Router();
+const knex = require('../knex');
+const bcrypt = require('bcrypt');
+
+router.post('/users', (req, res, next) => {
+  bcrypt.hash(req.body.password, 12, (err, hashed_password) => {
+    if (err) {
+      return next(err);
+    }
+
+    knex('users')
+      .insert({
+        first_name: req.body.first_name,
+        last_name:req.body.last_name,
+        email: req.body.email,
+        hashed_password: hashed_password
+      }, '*')
+      .then((users) => {
+        res.sendStatus(200);
+      })
+      .catch((err) => {
+        next(err);
+      });
+  });
+});
+
+module.exports = router;
+
+
+
 //       .then((users) => {
 //         res.sendStatus(200);
 //       })

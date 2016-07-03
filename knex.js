85381b8b89330a5bfb2c3ba5e7a81c208980f0d6
Peter Doane <peterdoane@gmail.com>
2016-07-02 18:17:15 -0700

    prepare for heroku

--- knex.js
+++ knex.js
@@ -2,13 +2,7 @@
 
 const environment = process.env.NODE_ENV || 'development';
 const knexConfig = require('./knexfile')[environment];
-const knex = require('knex')(knexConfig);
+// const knex = require('knex')(knexConfig);
+const knex = require('knex')({client: 'pg', connection: 'postgres://qzzvlzfozkoeze:4MFih9CpuXhCtSU8kR03wyen6O@ec2-54-225-81-90.compute-1.amazonaws.com:5432/d8bnjteijkss8l' });
 
-module.exports = {
-  client:'pg',
-  connections: 'postgres://qzzvlzfozkoeze:4MFih9CpuXhCtSU8kR03wyen6O@ec2-54-225-81-90.compute-1.amazonaws.com:5432/d8bnjteijkss8l',
-  pool:{
-    min:0,
-    max:7
-  }
-};
+module.exports = knex;

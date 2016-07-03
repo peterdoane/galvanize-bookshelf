1ee0dc4047970b77ce6f3b2ba820f8d8b2ace75d
Peter Doane <peterdoane@gmail.com>
2016-07-02 18:06:57 -0700

    prepare for heroku

--- knex.js
+++ knex.js
@@ -4,4 +4,7 @@ const environment = process.env.NODE_ENV || 'development';
 const knexConfig = require('./knexfile')[environment];
 const knex = require('knex')(knexConfig);
 
-module.exports = knex;
+module.exports = {
+  client:'pg',
+  connections: process.env.DATABASE_URL
+};
